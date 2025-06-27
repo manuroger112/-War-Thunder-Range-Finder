3acc926973e10b363773c8948d3209238f4b191f
manuroger112 <135890182+manuroger112@users.noreply.github.com>
2025-06-27 23:19:35 +0200

    Add files via upload

---
+++ RangeFinder.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+import math
+
+root = tk.Tk()
+root.geometry("400x400")
+
+root.attributes('-alpha', 0.5)
+root.title("manuroger112's range calculator")
+
+canvas = tk.Canvas(root, width=600, height=600, bg= "black")
+canvas.pack()
+
+class gls:
+    pxLabelOffset = 10
+    Distance = 200.0
+    NormUnit = 250.0
+    Range = (Distance/NormUnit) * 250
+    PrecisionCoefficient = 0.9
+    
+Globals = gls()
+
+def BeginDrag(event):
+    #get the specific widget of the event that we are dealing with
+    widget = event.widget
+    
+    widget.beginX = event.x
+    widget.beginY = event.y
+
+def MouseDrag(event):
+    widget = event.widget
+    
+    x = widget.winfo_x() - widget.beginX + event.x
+    y = widget.winfo_y() - widget.beginY + event.y
+    widget.place(x=x, y=y)
+
+    canvas.coords(RangeLine, Point1.winfo_x()+Globals.pxLabelOffset, Point1.winfo_y()+Globals.pxLabelOffset, Point2.winfo_x()+Globals.pxLabelOffset, Point2.winfo_y() + Globals.pxLabelOffset)
+    Globals.Distance = round(math.sqrt(math.pow((Point2.winfo_x() - Point1.winfo_x()), 2)+math.pow((Point2.winfo_y() - Point1.winfo_y()), 2)))
+    Globals.Range = round(((Globals.Distance/Globals.NormUnit) * 250) * Globals.PrecisionCoefficient) #times 0.9 as 0.9 is coefficient found through studies in wt of how much the calculated range is in comparison to actual range over different resolutions
+    TextDistance.config(text=f"Distance: {Globals.Distance} pixels | Range = {Globals.Range} Meters")
+
+def SetNormalizer():
+    
+    Globals.NormUnit = abs(Point2.winfo_x() - Point1.winfo_x())
+    NormalizedUnit.config(text=f"Normalized Unit = {Globals.NormUnit} pixels for 250 Meters")
+
+Point1 = tk.Label(canvas, text="+", width = 2, height = 1, bg="green")
+Point2 = tk.Label(canvas, text="+", width = 2, height = 1, bg="red")
+
+TextDistance = tk.Label(canvas, text=f"Distance:{Globals.Distance} pixels | Range = {Globals.Range} Meters", relief=tk.RAISED, font=("Impact", 12, "italic"), width = 40, height = 1);
+TextDistance.place(x=0, y=0)
+
+SetUnitNormalizer = tk.Button(root, text="Set Unit Norm", width = 10, font=("Impact", 9, ""), height = 2, command=SetNormalizer)
+SetUnitNormalizer.place(x=240, y=30)
+
+NormalizedUnit = tk.Label(root, text=f"Normalized Unit = {Globals.NormUnit} pixels for 250 Meters", font=("Impact", 8, "italic"))
+NormalizedUnit.place(x=0, y= 25)
+
+Point1.place(x=0, y=200)
+Point2.place(x=200, y=200)
+
+Point1.bind("<Button-1>", BeginDrag)
+Point1.bind("<B1-Motion>", MouseDrag)
+
+Point2.bind("<Button-1>", BeginDrag)
+Point2.bind("<B1-Motion>", MouseDrag)
+
+
+RangeLine = canvas.create_line(0+Globals.pxLabelOffset, 200+Globals.pxLabelOffset, 200, 200+Globals.pxLabelOffset, fill="green", width=1)
+
+root.mainloop()
